5e21e686f67cd32f8b3a1faa723edf263c10e651
matiastesho <87156480+matiastesho@users.noreply.github.com>
2021-07-08 21:27:43 -0300

    Add files via upload

---
+++ plugins/preloader/preloader.js
@@ -0,0 +1,20 @@
+	$(window).on("load", function(){			
+		//se cargaron todos los recursos, librerías e imágenes
+		
+		//a la caja con clase .precarga, después de 1.5s, corre el efecto fadeOut
+		$(".precarga").delay(1500).fadeOut("normal", function(){
+			//cuando finalice la transicion, se disparan las siguientes instrucciones
+			$(this).remove(); //remover este elemento del DOM
+		});		
+	
+		console.log("todos los recurso están");
+	});
+	
+	
+	// lo mismo que $(document).ready() pero más cool
+	$(function(){
+		console.log("el documento está listo");
+		
+		//instrucciones jquery, inicializaciones de plugins, etc
+	
+	});
